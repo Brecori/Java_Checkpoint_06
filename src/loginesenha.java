d302708b2391ba3bcdf63285a2da3fb87fee7de5
Breno Tosi <101286015+Brecori@users.noreply.github.com>
2022-10-18 20:43:28 -0300

    Primeiro EX realizado!

---
+++ src/loginesenha.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class loginesenha {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nome;
+        String nomeInicial;
+        String nameToChar;
+        String NOMES[] = new String[1];
+        String login = "";
+        char senha[] = new char[12];
+        String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i;
+
+        System.out.print("Insira o seu nome completo: ");
+        nomeInicial = input.nextLine();
+
+        nome = nomeInicial;
+        nome = nome.replace(" das ", " da ");
+        nome = nome.replace(" dos ", " do ");
+        nome = nome.replaceAll(" d[aeo] ", " ");
+        NOMES = nome.split(" ");
+        int ultimoNome = NOMES.length - 1;
+
+        if (NOMES.length > 2) {
+            for (i = 0; i < NOMES.length; i++) {
+                if (i < 2) {
+                    login = login.concat(String.valueOf(NOMES[i].charAt(0)));
+                }
+
+            }
+            login = login.concat(NOMES[ultimoNome]).toLowerCase();
+        }
+        else {
+            login = login.concat(String.valueOf(NOMES[0].charAt(0)));
+            login = login.concat(NOMES[ultimoNome]).toLowerCase();
+        }
+        System.out.println();
+        System.out.println("\tSeu Nome: \t" + nomeInicial);
+        System.out.println("\tSeu Login: \t" + login);
+
+
+
+
+
+
+
+
+
+
+
+        input.close();
+    }
+}
